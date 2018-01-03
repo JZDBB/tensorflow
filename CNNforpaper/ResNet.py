065739db1b8807a3f98c0c53c3e88b43d7ed16f1
Xuyining <oxuyining@gmail.com>
2018-01-03 11:23:34 +0800

    suibian

--- CNNforpaper/ResNet.py
+++ CNNforpaper/ResNet.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import numpy as np
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.training import moving_averages
 
 def weight_variable(shape):
   initial = tf.random_normal(shape, stddev=0.01)
@@ -8,8 +11,31 @@ def bias_variable(x, shape):
     initial = tf.constant(x, shape=shape)
     return tf.Variable(initial)
 
-def batch_norm(x):
-    return x
+def batch_norm(x, is_training):
+    x_shape = x.get_shape()
+    params_shape = x_shape[-1:]
+
+    axis = list(range(len(x_shape) - 1))
+
+    beta = tf.get_variable('beta', params_shape, initializer=tf.zeros_initializer())
+        # tf.Variable(tf.constant(tf.zeros_initializer(), shape=params_shape), 'beta')
+    gamma = tf.get_variable('gamma', params_shape, initializer=tf.ones_initializer())
+
+    moving_mean = tf.get_variable('moving_mean', params_shape, initializer=tf.zeros_initializer(), trainable=False)
+    moving_variance = tf.get_variable('moving_variance', params_shape, initializer=tf.ones_initializer(), trainable=False)
+
+    # These ops will only be preformed when training.
+    mean, variance = tf.nn.moments(x, axis)
+    update_moving_mean = moving_averages.assign_moving_average(moving_mean, mean, BN_DECAY)
+    update_moving_variance = moving_averages.assign_moving_average(moving_variance, variance, BN_DECAY)
+    tf.add_to_collection(UPDATE_OPS_COLLECTION, update_moving_mean)
+    tf.add_to_collection(UPDATE_OPS_COLLECTION, update_moving_variance)
+
+    mean, variance = control_flow_ops.cond(
+        is_training, lambda: (mean, variance),
+        lambda: (moving_mean, moving_variance))
+
+    return tf.nn.batch_normalization(x, mean, variance, beta, gamma, 'Batch_norm')
 
 def conv(x, Weight_shape, bias_val, bias_shape, stride):
     W_conv = weight_variable(Weight_shape)
