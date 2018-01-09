4816d5299d21bb044d345806607ab596f93e1cb1
Xuyining <oxuyining@gmail.com>
2018-01-09 17:18:57 +0800

    coonvert to tfrecords

--- CNNforpaper/ResNet/converet2tfrecord.py
+++ CNNforpaper/ResNet/converet2tfrecord.py
@@ -1,26 +1,69 @@
 import os
 import tensorflow as tf
-import numpy as np
 from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
 
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def read_data():
+    dict_train = unpickle('cifar-10-python\data_batch_1')
+    train_data = dict_train[b'data']
+    train_labels = dict_train[b'labels']
+    dict_train = unpickle('cifar-10-python\data_batch_2')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    dict_train = unpickle('cifar-10-python\data_batch_3')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    dict_train = unpickle('cifar-10-python\data_batch_4')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    dict_train = unpickle('cifar-10-python\data_batch_5')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    dict_test = unpickle('cifar-10-python\\test_batch')
+    test_data = dict_test[b'data']
+    test_labels = dict_test[b'labels']
+    train_data = np.reshape(train_data, [-1, 32, 32, 3], 'F')
+    train_data = np.transpose(train_data, [0, 2, 1, 3])
+    test_data = np.reshape(test_data, [-1, 32, 32, 3], 'F')
+    test_data = np.transpose(test_data, [0, 2, 1, 3])
+    return train_data, train_labels, test_data, test_labels
 
 classes={'iris','contact'}
-writer= tf.python_io.TFRecordWriter("iris_contact.tfrecords")
+train_data, train_labels, test_data, test_labels = read_data()
 
-for index,name in enumerate(classes):
-    class_path='/'
-    for img_name in os.listdir(class_path):
-        img_path=class_path+img_name
-        img=Image.open(img_path)
-        img= img.resize((512,80))
-        img_raw=img.tobytes()
-        #plt.imshow(img) # if you want to check you image,please delete '#'
-        #plt.show()
-        example = tf.train.Example(features=tf.train.Features(feature={
-            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
-            'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
-        }))
-        writer.write(example.SerializeToString())
+# writer= tf.python_io.TFRecordWriter("traindata.tfrecords")
+# for i in range(50000):
+#     img= train_data[i]
+#     # img= img.resize((32, 32))
+#     img_raw=img.tobytes()
+#     # plt.imshow(img)
+#     # plt.show()
+#     example = tf.train.Example(features=tf.train.Features(feature={
+#         "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[train_labels[i]])),
+#         'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+#     }))
+#     writer.write(example.SerializeToString())
+#
+# writer.close()
+
+writer= tf.python_io.TFRecordWriter("testdata.tfrecords")
+for i in range(10000):
+    img= test_data[i]
+    # img= img.resize((32, 32))
+    img_raw=img.tobytes()
+    # plt.imshow(img)
+    # plt.show()
+    example = tf.train.Example(features=tf.train.Features(feature={
+        "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[test_labels[i]])),
+        'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+    }))
+    writer.write(example.SerializeToString())
 
 writer.close()
