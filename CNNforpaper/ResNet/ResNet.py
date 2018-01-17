98083585cf0a60ac3d982b3f6a7015c95449b161
Xuyining <oxuyining@gmail.com>
2018-01-17 13:42:34 +0800

    train-ssdiannao

--- CNNforpaper/ResNet/ResNet.py
+++ CNNforpaper/ResNet/ResNet.py
@@ -16,11 +16,12 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.training import moving_averages
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
-BN_DECAY = 0.999
+BN_DECAY = 0.9
 Weight_decay = 0.0001
 
-def read_and_decode(filename):
+def read_and_decode(filename, train):
     #根据文件名生成一个队列
     filename_queue = tf.train.string_input_producer([filename])
 
@@ -35,6 +36,10 @@ def read_and_decode(filename):
     img = tf.decode_raw(features['img_raw'], tf.uint8)
     img = tf.reshape(img, [32, 32, 3])
     img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    if train:
+        img = tf.image.pad_to_bounding_box(img, 4, 4, 40, 40)
+        img = tf.random_crop(img, [32, 32, 3])
+        img = tf.image.random_flip_left_right(img)
     label = tf.cast(features['label'], tf.int32)
 
     return img, label
@@ -47,9 +52,10 @@ def bias_variable(x, shape):
     initial = tf.constant(x, shape=shape)
     return tf.Variable(initial)
 
+
 def batch_norm(x, is_training):
+    # return tf.contrib.layers.batch_norm(x, is_training=is_training)
     is_training = tf.convert_to_tensor(is_training)
-    global BN_DECAY
     x_shape = x.get_shape()
     params_shape = x_shape[-1:]
 
@@ -66,25 +72,15 @@ def batch_norm(x, is_training):
     mean, variance = tf.nn.moments(x, axis)
     update_moving_mean = moving_averages.assign_moving_average(moving_mean, mean, BN_DECAY)
     update_moving_variance = moving_averages.assign_moving_average(moving_variance, variance, BN_DECAY)
-    tf.add_to_collection('store_mean', update_moving_mean)
-    tf.add_to_collection('store_variance', update_moving_variance)
+    tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_moving_mean)
+    tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_moving_variance)
 
     mean, variance = control_flow_ops.cond(
         is_training, lambda: (mean, variance),
         lambda: (moving_mean, moving_variance))
 
-    return tf.nn.batch_normalization(x, mean, variance, beta, gamma, 0.0001, 'Batch_norm')
+    return tf.nn.batch_normalization(x, mean, variance, beta, gamma, 0.001, 'Batch_norm')
 
-def conv(x, Weight_shape, bias_val, bias_shape, stride):
-    W_conv = weight_variable(Weight_shape, math.sqrt(2.0))
-    b_conv = bias_variable(bias_val, bias_shape)
-    out_conv = tf.nn.conv2d(x, W_conv, strides=stride, padding="SAME") + b_conv
-    out_BN = batch_norm(out_conv, is_training=True)
-    return out_BN
-
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
 
 def deep(x, is_training):
     # with tf.name_scope("reshape"):
@@ -93,6 +89,7 @@ def deep(x, is_training):
     with tf.variable_scope("pre_conv"):
         with tf.variable_scope("conv"):
             W_preconv = weight_variable([3, 3, 3, 16], math.sqrt(2.0/(3*3*3)))
+            tf.add_to_collection('w', W_preconv)
             # b_preconv = bias_variable(0., [16])
             preconv = tf.nn.conv2d(x, W_preconv, strides=[1, 1, 1, 1],
                                    padding="SAME") #+ b_preconv
@@ -103,6 +100,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block1_16"):
         with tf.variable_scope("conv1"):
             W_conv1 = weight_variable([3, 3, 16, 16], math.sqrt(2./(3*3*16)))
+            tf.add_to_collection('w', W_conv1)
             # b_conv1 = bias_variable(0., [16])
             conv1 = tf.nn.conv2d(out_preconv, W_conv1, strides=[1, 1, 1, 1],
                                  padding="SAME") #+ b_conv1
@@ -112,6 +110,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv2"):
             W_conv2 = weight_variable([3, 3, 16, 16], math.sqrt(2./(3*3*16)))
+            tf.add_to_collection('w', W_conv2)
             # b_conv2 = bias_variable(0., [16])
             conv2 = tf.nn.conv2d(out_conv1, W_conv2, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv2
@@ -123,6 +122,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block2_16"):
         with tf.variable_scope("conv3"):
             W_conv3 = weight_variable([3, 3, 16, 16], math.sqrt(2./(3*3*16)))
+            tf.add_to_collection('w', W_conv3)
             # b_conv3 = bias_variable(0., [16])
             conv3 = tf.nn.conv2d(out_block1_16, W_conv3, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv3
@@ -132,6 +132,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv4"):
             W_conv4 = weight_variable([3, 3, 16, 16], math.sqrt(2./(3*3*16)))
+            tf.add_to_collection('w', W_conv4)
             # b_conv4 = bias_variable(0., [16])
             conv4 = tf.nn.conv2d(out_conv3, W_conv4, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv4
@@ -143,6 +144,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block3_16"):
         with tf.variable_scope("conv5"):
             W_conv5 = weight_variable([3, 3, 16, 16], math.sqrt(2./(3*3*16)))
+            tf.add_to_collection('w', W_conv5)
             # b_conv5 = bias_variable(0., [16])
             conv5 = tf.nn.conv2d(out_block2_16, W_conv5, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv5
@@ -152,6 +154,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv6"):
             W_conv6 = weight_variable([3, 3, 16, 16], math.sqrt(2./(3*3*16)))
+            tf.add_to_collection('w', W_conv6)
             # b_conv6 = bias_variable(0., [16])
             conv6 = tf.nn.conv2d(out_conv5, W_conv6, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv6
@@ -163,6 +166,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block1_32"):
         with tf.variable_scope("conv32-1"):
             W_conv32_1 = weight_variable([3, 3, 16, 32], math.sqrt(2./(3*3*16)))
+            tf.add_to_collection('w', W_conv32_1)
             # b_conv32_1 = bias_variable(0., [32])
             conv32_1 = tf.nn.conv2d(out_block3_16, W_conv32_1, strides=[1, 2, 2, 1],
                                  padding="SAME") #+ b_conv32_1
@@ -172,6 +176,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv32-2"):
             W_conv32_2 = weight_variable([3, 3, 32, 32], math.sqrt(2./(3*3*32)))
+            tf.add_to_collection('w', W_conv32_2)
             # b_conv32_2 = bias_variable(0., [32])
             conv32_2 = tf.nn.conv2d(out_conv32_1, W_conv32_2, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv32_2
@@ -180,9 +185,10 @@ def deep(x, is_training):
         with tf.variable_scope("shortcut1"):
             #option B
             W_SC1 = weight_variable([1, 1, 16, 32], math.sqrt(2./(1*1*16)))
-            b_SC1 = bias_variable(0., [32])
+            tf.add_to_collection('w', W_SC1)
+            # b_SC1 = bias_variable(0., [32])
             conv_SC1 = tf.nn.conv2d(out_block3_16, W_SC1, strides=[1, 2, 2, 1],
-                                    padding="SAME") + b_SC1
+                                    padding="SAME") #+ b_SC1
             out_SC1 = batch_norm(conv_SC1, is_training)
 
             #option A
@@ -197,6 +203,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block2_32"):
         with tf.variable_scope("conv32-3"):
             W_conv32_3 = weight_variable([3, 3, 32, 32], math.sqrt(2./(3*3*32)))
+            tf.add_to_collection('w', W_conv32_3)
             # b_conv32_3 = bias_variable(0., [32])
             conv32_3 = tf.nn.conv2d(out_block1_32, W_conv32_3, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv32_3
@@ -206,6 +213,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv32-4"):
             W_conv32_4 = weight_variable([3, 3, 32, 32], math.sqrt(2./(3*3*32)))
+            tf.add_to_collection('w', W_conv32_4)
             # b_conv32_4 = bias_variable(0., [32])
             conv32_4 = tf.nn.conv2d(out_conv32_3, W_conv32_4, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv32_4
@@ -218,6 +226,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block3_32"):
         with tf.name_scope("conv32-5"):
             W_conv32_5 = weight_variable([3, 3, 32, 32], math.sqrt(2./(3*3*32)))
+            tf.add_to_collection('w', W_conv32_5)
             # b_conv32_5 = bias_variable(0., [32])
             conv32_5 = tf.nn.conv2d(out_block2_32, W_conv32_5, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv32_5
@@ -227,6 +236,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv32-6"):
             W_conv32_6 = weight_variable([3, 3, 32, 32], math.sqrt(2./(3*3*32)))
+            tf.add_to_collection('w', W_conv32_6)
             # b_conv32_6 = bias_variable(0., [32])
             conv32_6 = tf.nn.conv2d(out_conv32_5, W_conv32_6, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv32_6
@@ -238,6 +248,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block1_64"):
         with tf.variable_scope("conv64-1"):
             W_conv64_1 = weight_variable([3, 3, 32, 64], math.sqrt(2./(3*3*32)))
+            tf.add_to_collection('w', W_conv64_1)
             # b_conv64_1 = bias_variable(0., [64])
             conv64_1 = tf.nn.conv2d(out_block3_32, W_conv64_1, strides=[1, 2, 2, 1],
                                  padding="SAME")# + b_conv64_1
@@ -247,6 +258,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv64-2"):
             W_conv64_2 = weight_variable([3, 3, 64, 64], math.sqrt(2./(3*3*32)))
+            tf.add_to_collection('w', W_conv64_2)
             # b_conv64_2 = bias_variable(0., [64])
             conv64_2 = tf.nn.conv2d(out_conv64_1, W_conv64_2, strides=[1, 1, 1, 1],
                                  padding="SAME")# + b_conv64_2
@@ -255,9 +267,10 @@ def deep(x, is_training):
         with tf.variable_scope("shortcut2"):
             #option B
             W_SC2 = weight_variable([1, 1, 32, 64], math.sqrt(2./(1*1*32)))
-            b_SC2 = bias_variable(0., [64])
+            tf.add_to_collection('w', W_SC2)
+            # b_SC2 = bias_variable(0., [64])
             conv_SC2 = tf.nn.conv2d(out_block3_32, W_SC2, strides=[1, 2, 2, 1],
-                                    padding="SAME") + b_SC2
+                                    padding="SAME") #+ b_SC2
             out_SC2 = batch_norm(conv_SC2, is_training)
 
             # option A
@@ -270,6 +283,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block2_64"):
         with tf.name_scope("conv64-3"):
             W_conv64_3 = weight_variable([3, 3, 64, 64], math.sqrt(2./(3*3*64)))
+            tf.add_to_collection('w', W_conv64_3)
             # b_conv64_3 = bias_variable(0., [64])
             conv64_3 = tf.nn.conv2d(out_block1_64, W_conv64_3, strides=[1, 1, 1, 1],
                                     padding="SAME") #+ b_conv64_3
@@ -279,6 +293,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv64-4"):
             W_conv64_4 = weight_variable([3, 3, 64, 64], math.sqrt(2./(3*3*64)))
+            tf.add_to_collection('w', W_conv64_4)
             # b_conv64_4 = bias_variable(0., [64])
             conv64_4 = tf.nn.conv2d(out_conv64_3, W_conv64_4, strides=[1, 1, 1, 1],
                                     padding="SAME")# + b_conv64_4
@@ -290,6 +305,7 @@ def deep(x, is_training):
     with tf.variable_scope("Block3_64"):
         with tf.variable_scope("conv64-5"):
             W_conv64_5 = weight_variable([3, 3, 64, 64], math.sqrt(2./(3*3*64)))
+            tf.add_to_collection('w', W_conv64_5)
             # b_conv64_5 = bias_variable(0., [64])
             conv64_5 = tf.nn.conv2d(out_block2_64, W_conv64_5, strides=[1, 1, 1, 1],
                                     padding="SAME") #+ b_conv64_5
@@ -299,6 +315,7 @@ def deep(x, is_training):
 
         with tf.variable_scope("conv64-6"):
             W_conv64_6 = weight_variable([3, 3, 64, 64], math.sqrt(2./(3*3*64)))
+            tf.add_to_collection('w', W_conv64_6)
             # b_conv64_6 = bias_variable(0., [64])
             conv64_6 = tf.nn.conv2d(out_conv64_5, W_conv64_6, strides=[1, 1, 1, 1],
                                     padding="SAME")# + b_conv64_6
@@ -312,10 +329,13 @@ def deep(x, is_training):
                         strides=[1, 2, 2, 1], padding='VALID')
 
     with tf.variable_scope("fc"):
-        W_fc = weight_variable([64, 10], math.sqrt(2./(64)))
+        W_fc = weight_variable([64, 10], math.sqrt(2./(640)))
+        tf.add_to_collection('w', W_fc)
         b_fc = bias_variable(0., [10])
         reshape = tf.reshape(pooling, [-1, 64])
         y_conv = tf.matmul(reshape, W_fc) + b_fc
+
+        # y_conv = batch_norm(y_conv, is_training)
     # for v in tf.get_all_trainable_variables()
     return y_conv
 
@@ -323,10 +343,11 @@ def main():
     global Weight_decay
     is_train = True
 
-    img_train, label_train = read_and_decode("traindata.tfrecords")
-    img_test, label_test = read_and_decode("testdata.tfrecords")
+    img_train, label_train = read_and_decode("traindata.tfrecords", True)
+    img_test, label_test = read_and_decode("testdata.tfrecords", False)
 
     x = tf.placeholder(tf.float32, [None, 32, 32, 3], name='input_image')
+
     y = tf.placeholder(tf.int32, [None], name='label')
     is_training = tf.placeholder(tf.bool)
 
@@ -334,8 +355,8 @@ def main():
 
     with tf.name_scope('weight_loss'):
         weights_norm = tf.reduce_sum(
-            input_tensor=Weight_decay * tf.concat(
-                [tf.nn.l2_loss(i) for i in tf.trainable_variables()], axis=0),
+            input_tensor=Weight_decay * tf.stack(
+                [tf.nn.l2_loss(i) for i in tf.get_collection('w')], axis=0),
             name='weights_norm'
         )
 
@@ -347,7 +368,9 @@ def main():
     with tf.name_scope('optimize'):
         global_step = tf.Variable(0, trainable=False)
         lr = tf.train.piecewise_constant(global_step, [32000, 48000], [0.1, 0.01, 0.001])
-        train_step = tf.train.MomentumOptimizer(lr, 0.9).minimize(loss)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_step = tf.train.MomentumOptimizer(lr, 0.9).minimize(loss)
 
     with tf.name_scope('accuracy'):
         correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.cast(y, tf.int64))
@@ -362,7 +385,7 @@ def main():
     store_mean = tf.get_collection('store_mean')
     store_variance = tf.get_collection('store_variance')
     list_add = store_mean + store_variance
-
+    list_add = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.Session() as sess:
         writer = tf.summary.FileWriter("./logs", sess.graph)
         writer.flush()
@@ -373,21 +396,24 @@ def main():
         threads = tf.train.start_queue_runners(coord=coord)
         for i in range(64000):
             img, labels = sess.run([img_batch_train, label_batch_train])
+            # for i in range(128):
+            #     a = (img[i, :]*255+128).astype(np.uint8)
+            #     plt.imshow(a)
+            #     plt.show()
             if i % 100 == 0:
-                feed_dict = {x:img, y:labels, is_training:True}
+                feed_dict = {x: img, y: labels, is_training: False}
                 train_accuracy = accuracy.eval(feed_dict)
                 img_t, labels_t = sess.run([img_batch_test, label_batch_test])
                 feed_dict = {x: img_t, y: labels_t, is_training: False}
                 test_accuracy = accuracy.eval(feed_dict)
                 print('step %d, training accuracy %g , validation accuracy %g' % (i, train_accuracy, test_accuracy))
-            train_step.run(feed_dict={x:img, y:labels, is_training:True})
-            sess.run(list_add, feed_dict={x:img, is_training:True})
+            sess.run(train_step, feed_dict={x: img, y: labels, is_training: True})
+            # sess.run(list_add, feed_dict={x: img, is_training: True})
         coord.request_stop()
         coord.join(threads)
 
-        img_batch_test, label_batch_test = tf.train.shuffle_batch([img_test, label_test],
-                                                                  batch_size=10000, capacity=50000,
-                                                                  min_after_dequeue=10000)
+        img_batch_test, label_batch_test = tf.train.batch([img_test, label_test],
+                                                                  batch_size=10000, capacity=50000)
         img_t, labels_t = sess.run([img_batch_test, label_batch_test])
         print('test accuracy %g' % accuracy.eval(feed_dict={x: img_t, y: labels_t, is_training:False}))
 
