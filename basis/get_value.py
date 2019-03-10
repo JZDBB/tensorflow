984e2ff1e9a461a3ea0de5f75cc435e6cfae6bde
XuYining <oxuyining@gmail.com>
2019-03-10 14:59:02 +0800

    get tensors value
    Signed-off-by: XuYining <oxuyining@gmail.com>

---
+++ basis/get_value.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+img = cv2.imread("1.jpg")
+img = cv2.resize(img, (224, 224))
+input = np.array([img])
+a = tf.placeholder(tf.float32, (1, 224, 224, 3), name='value')
+
+def net(x):
+    y = tf.layers.conv2d(a, 64, 3, name='conv1')
+    return tf.layers.conv2d(y, 3, 3, name='conv2')
+
+b = net(a)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(b, feed_dict={a:input})
+    writer = tf.summary.FileWriter('./logs', sess.graph)
+    writer.close()
+    c = tf.get_default_graph().get_tensor_by_name(name='conv1/BiasAdd:0')
+    print(sess.run(c, feed_dict={a:input}))
+    d = 1
